1b9bc907f6b3d43dfd4567ad40a774a565edbbe3
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-01-14 11:54:28 +0800

    second commit

--- boc/api/logs/log-2016-10-23.php
+++
@@ -1,8 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2016-10-23 02:57:00 --> Language file contains no data: language/zh_CN/common_lang.php
-ERROR - 2016-10-23 12:17:59 --> Language file contains no data: language/zh_CN/common_lang.php
-ERROR - 2016-10-23 15:41:46 --> Language file contains no data: language/zh_CN/common_lang.php
-ERROR - 2016-10-23 16:43:15 --> Language file contains no data: language/zh_CN/common_lang.php
-ERROR - 2016-10-23 16:43:46 --> Language file contains no data: language/zh_CN/common_lang.php
-ERROR - 2016-10-23 16:44:16 --> Language file contains no data: language/zh_CN/common_lang.php
--- boc/api/logs/log-2016-11-05.php
+++
@@ -1,3 +0,0 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
-
-ERROR - 2016-11-05 01:19:21 --> Language file contains no data: language/zh_CN/common_lang.php
