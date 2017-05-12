5181d82695028fe5baf5fe57e17be3a759c46371
zhangzhiwei <zhangzhiwei1228@gmail.com>
2017-05-12 16:18:48 +0800

    我的浏览记录

--- boc/api/controllers/browse.php
+++ boc/api/controllers/browse.php
@@ -16,8 +16,7 @@ class browse extends API_Controller
         parent::__construct();
         $this->_auto();
         $this->load->model('browse_model', 'mbrowse');
-
-
+        $this->load->model('product_model', 'mproduct');
     }
     public function blist() {
         //type 1产品2视频3动态
@@ -26,23 +25,29 @@ class browse extends API_Controller
             case 1:
                 $join = array('product','product.id=browse.rid','left');
                 $title = $this->data['language'] == 'ZH' ? 'product.title' : 'product.'.$this->data['language'] . '_title';
-                $Fields = 'browse.id,' . $title . ',product.photo,browse.timeline';
+                $Fields = 'browse.id,' . $title . ',product.photo,browse.timeline,browse.type,product.id as pid';
                 break;
             case 2:
                 $join = array('videos','videos.id=browse.rid','left');
                 $title = $this->data['language'] == 'ZH' ? 'videos.title' : 'videos.'.$this->data['language'] . '_title';
-                $Fields = 'browse.id,' . $title . ',videos.photo,browse.timeline';
+                $Fields = 'browse.id,' . $title . ',videos.photo,browse.timeline,browse.type,product.id as pid';
                 break;
             case 3:
                 $join = array('news','news.id=browse.rid','left');
                 $title = $this->data['language'] == 'ZH' ? 'news.title' : 'news.'.$this->data['language'] . '_title';
-                $Fields = 'browse.id,' . $title . ',news.photo,browse.timeline';
+                $Fields = 'browse.id,' . $title . ',news.photo,browse.timeline,browse.type,product.id as pid';
                 break;
         }
-        $where = array('uid'=>$this->userinfo['id']);
+        $where = array('uid'=>$this->userinfo['id'],'type'=>$type);
         $orderby = array('timeline'=>'desc');
         $this->_list();
         if ($list = $this->mbrowse->get_list($this->limit, $this->offset, $orderby, $where, $Fields,false,$join)) {
+            foreach($list as &$row) {
+                if($row['type'] == 1) {
+                    $childs = $this->mproduct->get_all(array('pid'=>$row['pid']));
+                    $row['is_file'] = $childs ? 1 : 0;
+                }
+            }
             photo2url($list);
             //$this->mproduct->get_count_all($where);
             $this->vdata['returnCode'] = '200';
